952204d2a23cda793477f6af3b4d61cf6d31c529
Francois van der Merwe <francois.vdmerwe@entelect.co.za>
2016-08-30 22:57:16 +0200

    Setup union template project

---
+++ Union/js/index.js
@@ -0,0 +1,94 @@
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+(function ($) {
+
+    var rsvpMessages = ["Ek wil ook daar wees!", "Sien julle daar!", "Tel my in!", "Waarvoor wag ons!", "Sit my naam op daardie lys!"];
+
+    $(window).on("load" , function () {
+        $("#preloaderContainer").fadeOut(400);
+		$("#preloader").delay(100).fadeOut(400, function() {
+		    $("body").removeClass("loading");
+
+		    if ($("#RsvpModel_Sent").val() === "True") {
+		        window.setTimeout(function() {
+		            $("#sentModal").modal("show");
+		        }, 100);
+		    }
+		});
+	});
+
+	$(document).ready(function () {
+
+	    (function() {
+	        var i = 0;
+	        var buttons = $(".btn-rsvp");
+
+	        for (i = 0; i < buttons.length; i++) {
+	            $(buttons[i]).html(rsvpMessages[getRandomIntInclusive(0, rsvpMessages.length)]);
+	        }
+	    })();
+
+	    $("a[href*='#']").not(".no-smooth-scroll").bind("click", function(e){
+           
+			var anchor = $(this);
+			$("html, body").stop().animate({
+				scrollTop: $(anchor.attr("href")).offset().top - $("nav").height()
+			}, 1000);
+			e.preventDefault();
+		});
+
+	    $("body").scrollspy({
+	        target: ".navbar-custom",
+	        offset: 70
+	    });
+
+		$(window).resize(function(){
+		    $("#DateCountdown").TimeCircles().rebuild();
+		});
+
+		$("form").submit(function(e) {
+		    $(".btn").prop("disabled", true);
+		    $("div.modal-overlay").removeClass("hidden");
+		});
+
+		$("#DateCountdown").TimeCircles({
+		    "animation": "smooth",
+		    "bg_width": 0.2,
+		    "fg_width": 0.03,
+		    "circle_bg_color": "#90989F",
+		    "time": {
+		        "Days": {
+		            "text": "Dae",
+		            "color": "#ffffff",
+		            "show": true
+		        },
+		        "Hours": {
+		            "text": "Ure",
+		            "color": "#ffffff",
+		            "show": true
+		        },
+		        "Minutes": {
+		            "text": "Minute",
+		            "color": "#ffffff",
+		            "show": true
+		        },
+
+		        "Seconds": {
+		            "text": "Sekondes",
+		            "color": "#ffffff",
+		            "show": true
+		        }
+		    }
+		});
+
+		window.setTimeout(function() {
+		    $("#ons").show();
+		    $("#troue").show();
+		    $("footer").show();
+		}, 1000);
+	    
+	});
+
+})(jQuery);
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/// <binding BeforeBuild='less' AfterBuild='zip' />
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var plumber = require("gulp-plumber");
+var zip = require("gulp-zip");
+
+gulp.task("less", function () {
+    return gulp.src("./Union/css/less/*.less")
+    .pipe(plumber())
+      .pipe(less({
+          paths: [path.join(__dirname, "less", "includes")]
+      }))
+      .pipe(gulp.dest("./Union/css/"));
+});
+
+gulp.task("zip", function () {
+    return gulp.src(["./Union/**", "!./Union/*.config"])
+        .pipe(zip("UnionTemplate.zip"))
+        .pipe(gulp.dest("./UnionDistribution"));
+});
